218eb200bd86ef9ce9e894a34917c4fe6de6e15f
DESKTOP-8KEIGJG\Landanan <landanan@yahoo.com>
2021-06-02 02:06:09 +0300

    adding validator

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -60,7 +60,7 @@ export class RecipeEditComponent implements OnInit {
     }
     
     this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
+      'name': new FormControl(recipeName, Validators.required),
       'imagePath': new FormControl(imageUrl),
       'description': new FormControl(description),
       'ingredients': recipeIngredients
